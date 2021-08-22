bf4373070a2609db0cc255e7e9be31c4ef6186a7
Geoff McDonald <glmcdona@gmail.com>
2021-08-22 11:01:37 -0700

    Add initial game, env, and training layout.

---
+++ game/game.py
@@ -0,0 +1,5 @@
+
+# TODO: Implement game class, class for each unit and structure, etc. Base it off for example:
+# https://github.com/Lux-AI-Challenge/Lux-Design-2021/blob/master/kits/python/simple/lux/game_objects.py
+# https://github.com/Lux-AI-Challenge/Lux-Design-2021/blob/master/kits/python/simple/lux/game_map.py
+# https://github.com/Lux-AI-Challenge/Lux-Design-2021/blob/master/kits/python/simple/lux/game.py
