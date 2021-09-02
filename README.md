9c904e148e63e8de32d4989c0e12df34fb4b4f82
Geoff McDonald <glmcdona@gmail.com>
2021-09-01 22:36:57 -0700

    Update readme. Start work on vision model.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # LuxPythonEnvGym
 Matching python environment code for Lux AI 2021 Kaggle competition, and a gym interface for RL models.
+
+This is still in-progress and not ready for use yet. I'll post official docs and usage guide here once a simple example converges, and can be transformed to a kaggle-submission format.
+
+
+| **Features**                         | **LuxAi2021** |
+| ------------------------------------ | ----------------------|
+| Lux game engine porting to python    | :heavy_check_mark: |
+| Documentation                        | :x: |
+| All actions supported                | :x: |
+| PPO example training agent           | :heavy_check_mark:  |
+| Example agent converges to a good policy | :x: |
+| Kaggle submission format agents      | :x: |
+| Lux replay viewer support            | :x: |
+| Game engine consistency validation to base game       | :x: |
+
+## Installation
+See above, this project isn't complete yet. If you want to try your own agents anyways, here are instructions. This should work cross-platform, but I've only tested Windows 10 and Ubuntu.
+
+Install luxai2021 environment package by running the installer:
+
+```python setup.py install```
+
+Now edit and run your ML agent training code:
+
+```python ./examples/train.py```
+
+You can then run tensorboard to monitor the training:
+
+```tensorboard --logdir lux_tensorboard```
--- examples/train_vision.py
+++ examples/train_vision.py
@@ -15,32 +15,92 @@ from luxai2021.env.lux_env import LuxEnvironment
 from luxai2021.env.agent import Agent
 from luxai2021.game.constants import LuxMatchConfigs_Default
 from luxai2021.game.actions import *
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.utils import set_random_seed
 
 from functools import partial # pip install functools
+
+import torch as th
+from torch import nn
+
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+
+
+# Custom policy network for the shared feature extraction forward
+# https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
+class CustomCombinedExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Dict):
+        # We do not know features-dim here before going over all the items,
+        # so put something dummy for now. PyTorch requires calling
+        # nn.Module.__init__ before adding modules
+        super(CustomCombinedExtractor, self).__init__(observation_space, features_dim=1)
+
+        extractors = {}
+
+        total_concat_size = 0
+        # We need to know size of the output of this extractor,
+        # so go over all the spaces and compute output feature sizes
+        for key, subspace in observation_space.spaces.items():
+            if key.startswith("map") and isinstance(subspace, spaces.Box):
+                # Downsample the map. Assumes it is 32x32 for the largest map support. The smallest mapsize of 12x12 is commended for information sake.
+                #NOTE: Color channels are in subspace.shape[0] and it is 1
+                input_width = subspace.shape[1]
+                input_height = subspace.shape[2]
+                input_channels = subspace.shape[3]
+                filter_count = 32
+                print(subspace.shape)
+
+                extractors[key] = nn.Sequential(
+                    # 3x3x[num of map layers]. Largest map goes from [32x32xNCHANNELS] down to [32x32xNFILTERS]
+                    # Num parameters for layer = 3x3x11x32 = 3,168
+                    nn.Conv3d(subspace.shape[0], filter_count, kernel_size=(3,3,input_channels), padding=(2,2,0)), 
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.ReLU(),
+                    nn.MaxPool3d (kernel_size=(2,2,1)), # largest map (32x32xNFILTERS -> 16x16) or smallest map (12x12) -> (6x6)
+
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.ReLU(),
+                    nn.MaxPool3d (kernel_size=(2,2,1)), # largest map (32x32xNFILTERS -> 8x8xNFILTERS) or smallest map (12x12xNFILTERS) -> (3x3xNFILTERS)
+
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.Conv3d(filter_count, filter_count, kernel_size=(3,3,1), padding='same'),
+                    nn.ReLU(),
+                    nn.Flatten(),
+                )
+                print(extractors[key])
+                print("Flattened size from CNN: %i" % ((input_width // 2 // 2) * (input_height // 2 // 2) * filter_count))
+                total_concat_size += (input_width // 2 // 2) * (input_height // 2 // 2) * filter_count
+            elif isinstance(subspace, spaces.Box):
+                # Run through a simple MLP
+                extractors[key] = nn.Flatten()
+                total_concat_size += np.prod(subspace.shape) # Multiply all the axes together to get total size?
+            elif isinstance(subspace, spaces.Discrete):
+                # Run through a simple MLP
+                extractors[key] = nn.Flatten()
+                total_concat_size += subspace.n
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+
+    def forward(self, observations) -> th.Tensor:
+        encoded_tensor_list = []
+
+        # self.extractors contain nn.Modules that do all the processing.
+        for key, extractor in self.extractors.items():
+            encoded_tensor_list.append(extractor(observations[key]))
+        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+        return th.cat(encoded_tensor_list, dim=1)
+
 
 # https://codereview.stackexchange.com/questions/28207/finding-the-closest-point-to-a-list-of-points
 def closest_node(node, nodes):
     dist_2 = np.sum((nodes - node)**2, axis=1)
     return np.argmin(dist_2)
-
-# https://stable-baselines3.readthedocs.io/en/master/guide/examples.html?highlight=SubprocVecEnv#multiprocessing-unleashing-the-power-of-vectorized-environments
-def make_env(env, rank, seed=0):
-    """
-    Utility function for multiprocessed env.
-
-    :param env_id: (str) the environment ID
-    :param num_env: (int) the number of environments you wish to have in subprocesses
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
-    """
-    def _init():
-        env.seed(seed + rank)
-        return env
-    set_random_seed(seed)
-    return _init
 
 class AgentPolicy(Agent):
     def __init__(self, mode="train", model=None) -> None:
@@ -83,9 +143,37 @@ class AgentPolicy(Agent):
         #   2x citytile counts [cur player, opponent]
         #   2x worker counts [cur player, opponent]
         #   2x cart counts [cur player, opponent]
-        self.observation_shape = (5+5+1+1+1+2+2+2, )
-        self.observation_space = spaces.Box(low=0, high=1, shape=
-                        self.observation_shape, dtype=np.float16)
+        self.observation_space_dict = {
+            #  Unit specific observations
+            "unit_direction_nearest_wood": spaces.Discrete(5),
+            "unit_direction_nearest_city": spaces.Discrete(5),
+            "unit_cargo": spaces.Box(low=0, high=1, shape=(1,)),
+
+            # Game state observations
+            "game_is_night": spaces.Discrete(2),
+            "game_percent_done": spaces.Box(low=0, high=1, shape=(1,), dtype=np.float16),
+            "game_citytile_counts": spaces.Box(low=0, high=1, shape=(2,), dtype=np.float16),
+            "game_worker_counts": spaces.Box(low=0, high=1, shape=(2,), dtype=np.float16),
+            "game_cart_counts": spaces.Box(low=0, high=1, shape=(2,), dtype=np.float16),
+
+            # Map of the game, organize as maximum map size (32x32xnum_layers)
+            # Layers: 11x
+            #   OpponentCarts
+            #   OpponentCities
+            #   OpponentWorkers
+            #   MyCarts
+            #   MyCities
+            #   MyWorkers
+            #   CityFuelOrUnitCargo
+            #   Cooldown
+            #   Resources: Wood
+            #   Resources: Coal
+            #   Resources: Uranium
+            #   RoadLevel
+            "map": spaces.Box(low=0, high=1, shape=(1, 32, 32, 11), dtype=np.float16) # 32x32x11 = 11,264
+        }
+
+        self.observation_space = spaces.Dict(self.observation_space_dict)
 
 
     def getAgentType(self):
@@ -173,17 +261,22 @@ class AgentPolicy(Agent):
                         )
 
         # Observation space: (Basic minimum for a wood miner agent)
-        # Unit:
-        #   5x direction_nearest_wood
-        #   5x direction_nearest_city
-        #   1x cargo size
-        # State:
-        #   1x is night
-        #   1x percent of game done
-        #   2x citytile counts [cur player, opponent]
-        #   2x worker counts [cur player, opponent]
-        #   2x cart counts [cur player, opponent]
-        obs = np.zeros(self.observation_shape)
+        obs = OrderedDict(
+            [
+                ("unit_direction_nearest_wood", 0), # "unit_direction_nearest_wood": spaces.Discrete(5),
+                ("unit_direction_nearest_city", 0), # "unit_direction_nearest_city": spaces.Discrete(5),
+                ("unit_cargo", np.zeros((1,))),     # "unit_cargo": spaces.Box(low=0, high=1, shape=(1,)),
+                ("game_is_night", 0),               # "game_is_night": spaces.Discrete(2),
+                ("game_percent_done", np.zeros((1,))), # "game_percent_done": spaces.Box(low=0, high=1, shape=(1,), dtype=np.float16),
+                ("game_citytile_counts", np.zeros((2,))), # "game_citytile_counts": spaces.Box(low=0, high=1, shape=(2,), dtype=np.float16),
+                ("game_worker_counts", np.zeros((2,))),
+                ("game_cart_counts", np.zeros((2,))),
+                ("map", np.zeros((1,32,32,11))), # "map": spaces.Box(low=0, high=1, shape=(32, 32, 11), dtype=np.float16) # 32x32x11 = 11,264
+            ]
+        )
+        return obs
+
+        #  Unit specific observations
         if unit != None:
             # Encode the direction to the nearest wood
             #   5x direction_nearest_wood
@@ -351,21 +444,23 @@ if __name__ == "__main__":
     player = AgentPolicy(mode="train")
     
     # Train the model
-    #num_cpu = 4
-    #env = SubprocVecEnv([make_env(LuxEnvironment(configs, learningAgent=AgentPolicy(mode="train"), opponentAgent=Agent()), i) for i in range(num_cpu)])
     env = LuxEnvironment(configs, player, opponent)
+    policy_kwargs = dict(
+        features_extractor_class=CustomCombinedExtractor
+    )
     model = PPO("MlpPolicy",
         env,
         verbose=1,
         tensorboard_log="./lux_tensorboard/",
         learning_rate = 0.0001,
         gamma=0.995,
-        gae_lambda = 0.95
+        gae_lambda = 0.95,
+        policy_kwargs=policy_kwargs
     )
     print("Training model...")
     model.learn(total_timesteps=10000000)
     print("Done training model.")
-    
+
     # Inference the model
     print("Inferencing model policy with rendering...")
     obs = env.reset()
@@ -398,4 +493,3 @@ if __name__ == "__main__":
     model.learn(total_timesteps=2000)
     env.close()
     print("Done")
-    
