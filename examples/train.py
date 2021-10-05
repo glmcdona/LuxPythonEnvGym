ec636b4938e18ff110f880728fc3159fcd4116b8
Lysk <kroyer.github@gmail.com>
2021-10-04 20:55:22 -0700

    remove exit

--- examples/train.py
+++ examples/train.py
@@ -21,7 +21,7 @@ if sys.version_info < (3,7) or sys.version_info > (3,7):
     class style():
         YELLOW = '\033[93m'
     print(style.YELLOW+"Warning, you are using python" + str(sys.version_info.major) + "." + str(sys.version_info.minor) + ", to submit to kaggle consider switching to python3.7.")
-    sys.exit()
+
 
 # https://stable-baselines3.readthedocs.io/en/master/guide/examples.html?highlight=SubprocVecEnv#multiprocessing-unleashing-the-power-of-vectorized-environments
 def make_env(local_env, rank, seed=0):
